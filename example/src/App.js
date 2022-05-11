5668cbc0998cf9230fd76b0c7449b6ad4fa84eb4
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-05-11 21:08:35 +0300

    npm packages first example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-import { ExampleComponent } from 'khn-ui'
+import { Paragraph, Button } from 'khn-ui'
 import 'khn-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  return (
+    <>
+     <Paragraph text ="Hello" />
+     <Button text ="Click" onClick={() => alert('kaan')} />
+  </>
+
+)}
 
 export default App
